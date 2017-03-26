d3c446f203c6d67b467df656d8e80943200ae062
Arktoz <bjorakr@stud.ntnu.no>
2017-03-26 18:49:22 +0200

    Add files via upload

--- Heisprosjekt_lab15/main.go
+++ Heisprosjekt_lab15/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"strconv"
 	"sync"
 
 	. "./admin"
@@ -16,15 +14,13 @@ import (
 
 func main() {
 
-	//go run main.go -id=our_id
-
 	buttonPressedChan := make(chan Button)
 	floorSensorTriggeredChan := make(chan int)
 
 	liftInstructionChan := make(chan Instruction)
 
-	outgoingMessageChan := make(chan Message, 100) // Må være asynkron
-	incomingMessageChan := make(chan Message, 100) // ----"-------"---
+	outgoingMessageChan := make(chan Message, 100)
+	incomingMessageChan := make(chan Message, 100)
 
 	incomingBackupChan := make(chan BackUp, 100)
 	outgoingBackupChan := make(chan BackUp, 100)
@@ -34,17 +30,8 @@ func main() {
 	startTimerChan := make(chan string)
 	timeOutChan := make(chan string)
 
-	// NB NB NB!!!! HUSK Å SETT INN "SKRIV INN ID HER".
-	var id string
-	flag.StringVar(&id, "id", "", "id of this peer")
-	flag.Parse()
-
-	if id == "" {
-		fmt.Println("Main: You should have entered an ID. Now set to 0, no matter what the others are")
-		id = "0"
-	}
-
-	IDInput, _ := strconv.Atoi(id)
+	var IDInput int
+	fmt.Scanf("%d", &IDInput)
 
 	Elev_init()
 	var wg sync.WaitGroup
